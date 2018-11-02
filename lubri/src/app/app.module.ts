53273b2d6ae8d4adfa1b2f6a422d7f66bee9a4cb
Yanina Lara <yanina-lara@hotmail.com>
2018-11-01 22:20:06 -0300

    tercer subida

--- lubri/src/app/app.module.ts
+++ lubri/src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+const routes: Routes = [
+  { path: 'contacto', component: ContactoComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    FooterComponent
+    FooterComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
